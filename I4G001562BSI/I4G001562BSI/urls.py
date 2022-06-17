9b153ba76547ebc06d6a7c900e0c344edcf00e23
Iloabuchi-Collins <iloabuchicollins1000@gmail.com>
2022-06-17 14:50:39 +0100

    Django project for making blog posts.

--- I4G001562BSI/I4G001562BSI/urls.py
+++ I4G001562BSI/I4G001562BSI/urls.py
@@ -15,9 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from blog.views import home_view
 
 urlpatterns = [
-    path('admin/', admin.site.urls),  
-    path("", home_view)
+    path('admin/', admin.site.urls), 
 ]
